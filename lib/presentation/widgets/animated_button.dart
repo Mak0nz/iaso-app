8e030b0c1774fd07c2d1d021a58072aeecd99e06
Mak0nz <72095988+Mak0nz@users.noreply.github.com>
2025-08-16 11:22:34 +0200

    added click animation to 3d button

--- lib/presentation/widgets/animated_button.dart
+++ lib/presentation/widgets/animated_button.dart
@@ -1,13 +1,12 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
-
 import 'package:flutter/material.dart';
 
 class AnimatedButton extends StatefulWidget {
   final onTap;
   final text;
   final progressEvent;
-
-  const AnimatedButton({super.key, 
+  const AnimatedButton({
+    super.key,
     required this.onTap,
     required this.text,
     required this.progressEvent,
@@ -18,32 +17,47 @@ class AnimatedButton extends StatefulWidget {
 }
 
 class _AnimatedButtonState extends State<AnimatedButton> {
+  bool _isPressed = false;
+
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: widget.onTap,
-      borderRadius: BorderRadius.circular(15),
-      child: Ink(
+    return GestureDetector(
+      onTapDown: (_) => setState(() => _isPressed = true),
+      onTapUp: (_) {
+        setState(() => _isPressed = false);
+        widget.onTap?.call();
+      },
+      onTapCancel: () => setState(() => _isPressed = false),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 100),
         width: double.infinity,
         padding: const EdgeInsets.all(14),
+        transform: Matrix4.translationValues(
+            _isPressed ? 1 : 0, _isPressed ? 2 : 0, 0),
         decoration: BoxDecoration(
           color: Colors.blue.shade400,
           borderRadius: BorderRadius.circular(15),
           boxShadow: [
             BoxShadow(
               color: Colors.blue.shade600,
-              spreadRadius: 2,
-              offset: const Offset(2, 5),
+              spreadRadius: _isPressed ? 1 : 2,
+              offset: _isPressed ? const Offset(1, 2) : const Offset(2, 5),
             ),
           ],
         ),
-        child: Center( child: widget.progressEvent ? const CircularProgressIndicator(color: Colors.white,):
-          Text(widget.text, 
-            style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
-          ),
+        child: Center(
+          child: widget.progressEvent
+              ? const CircularProgressIndicator(color: Colors.white)
+              : Text(
+                  widget.text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
         ),
       ),
     );
-
   }
 }
